2c15ed1cf5010c8a38ed02643b97df9a652cbae5
Yaroslav Kobel <yarokobel@gmail.com>
2020-06-05 23:49:44 +0300

    recognize + add rectangle

--- faces.py
+++ faces.py
@@ -1,29 +1,37 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
 
+cap = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    out.write(frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        print(x, y, w, h)
+        roi_gray = gray[y:y + h, x:x + w]  # (ycord_start, ycord_end)
+        roi_color = frame[y:y + h, x:x + w]
+
+        # recognize?
+
+        img_item = "my-image.png"
+        cv2.imwrite(img_item, roi_gray)
+
+        color = (255, 0, 0) #BRG 0-255
+        stroke = 2
+        end_cord_x = x + w
+        end_cord_y = y + h
+        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
-<<<<<<< HEAD
-
-# When everything done, release the capture
-cap.release()
-out.release()
-cv2.destroyAllWindo
-qws()
-=======
 # When everything done, release the capture
 cap.release()
 out.release()
 cv2.destroyAllWindows()
->>>>>>> c476dc85e1e5552366a169f069ea40783fb9c2a1
