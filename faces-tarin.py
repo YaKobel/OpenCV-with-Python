c14f9d2c07634bc4051dc32cd43120abbba731b2
Yaroslav Kobel <yarokobel@gmail.com>
2020-06-10 16:14:31 +0300

    add resize Eyes and Smil

--- faces-tarin.py
+++ faces-tarin.py
@@ -4,11 +4,11 @@ import numpy as np
 from PIL import Image
 import pickle
 
-face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
-
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
+
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 current_id = 0
 label_ids = {}
@@ -20,25 +20,25 @@ for root, dirs, files in os.walk(image_dir):
         if file.endswith("png") or file.endswith("jpg"):
             path = os.path.join(root, file)
             label = os.path.basename(root).replace(" ", "-").lower()
-            print(label, path)
+            # print(label, path)
             if not label is label_ids:
                 label_ids[label] = current_id
                 current_id += 1
             id_ = label_ids[label]
-            print(label_ids)
-
+            # print(label_ids)
             # y_labels.append(label) # some number
             # x_train.append(path) # verify this image, turn into a NUMPY array, GRAY
             pill_image = Image.open(path).convert("L") # grayscale
+            size = (550, 550)
+            final_image = pill_image.resize(size, Image.ANTIALIAS)
             image_array = np.array(pill_image, "uint8")
-            print(image_array)
+            # print(image_array)
             faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
 
             for (x, y, w, h) in faces:
                 roi = image_array[y:y+h, x:x+w]
                 x_train.append(roi)
                 y_labels.append(id_)
-
 
 # print(y_labels)
 # print(x_train)
